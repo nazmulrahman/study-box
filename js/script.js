a5e5359cf80254089c7725fa1d36672b0c2ecce1
Nazmul Rahman <nazmulrahman1658@gmail.com>
2021-08-24 17:30:27 +0600

    college class section done

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//College class sheet show
+const clgSheet = document.getElementById("clg-sheet");
+        clgSheet.addEventListener("click", function(){
+            const embedArea = document.getElementById("college-class");
+            embedArea.style.display = "block";
+            
+        });
+//College class sheet hide
+//College class sheet show
+const clgX = document.getElementById("clg-x");
+        clgX.addEventListener("click", function(){
+            const embedArea = document.getElementById("college-class");
+            embedArea.style.display = "none";
+            
+        });
