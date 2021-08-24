4ed99e52d518d2e329c6ae102afb9477a08d86b2
Nazmul Rahman <nazmulrahman1658@gmail.com>
2021-08-24 19:05:47 +0600

    all embed section done

--- js/script.js
+++ js/script.js
@@ -6,10 +6,57 @@ const clgSheet = document.getElementById("clg-sheet");
             
         });
 //College class sheet hide
-//College class sheet show
 const clgX = document.getElementById("clg-x");
         clgX.addEventListener("click", function(){
             const embedArea = document.getElementById("college-class");
+            embedArea.style.display = "none";
+            
+        });
+//efb class sheet show
+const efbSheet = document.getElementById("efb-sheet");
+        efbSheet.addEventListener("click", function(){
+            const embedArea = document.getElementById("efb-class");
+            embedArea.style.display = "block";
+            
+        });
+//efb class sheet hide
+const efbX = document.getElementById("efb-x");
+        efbX.addEventListener("click", function(){
+            const embedArea = document.getElementById("efb-class");
+            embedArea.style.display = "none";
+            
+        });
+
+
+
+//assignment class sheet show
+const assSheet = document.getElementById("ass-sheet");
+        assSheet.addEventListener("click", function(){
+            const embedArea = document.getElementById("ass-class");
+            embedArea.style.display = "block";
+            
+        });
+//ass class sheet hide
+const assX = document.getElementById("ass-x");
+        assX.addEventListener("click", function(){
+            const embedArea = document.getElementById("ass-class");
+            embedArea.style.display = "none";
+            
+        });
+
+
+
+//math class sheet show
+const mathSheet = document.getElementById("math-sheet");
+        mathSheet.addEventListener("click", function(){
+            const embedArea = document.getElementById("math-class");
+            embedArea.style.display = "block";
+            
+        });
+//math class sheet hide
+const mathX = document.getElementById("math-x");
+        mathX.addEventListener("click", function(){
+            const embedArea = document.getElementById("math-class");
             embedArea.style.display = "none";
             
         });
